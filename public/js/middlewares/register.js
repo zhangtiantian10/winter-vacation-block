4a62a7907da81666461f7cdcf96a6aa28c92dc28
liyanyan19940914 <921258351@qq.com>
2017-02-16 17:24:22 +0800

    add middleware

--- public/js/middlewares/register.js
+++ public/js/middlewares/register.js
@@ -1,3 +1,17 @@
-/**
- * Created by liyan on 17-2-16.
- */
+import request from 'superagent';
+
+export default store => next => action => {
+    if(action.type === 'ADD_USENAME') {
+        request.post('/addUseName')
+            .send(action.data)
+            .end((err,res) => {
+                if(res.statusCode === 201) {
+                    alert("保存成功！");
+                } else {
+                    alert("保存失败！");
+                }
+            })
+    } else {
+        next(action);
+    }
+}
