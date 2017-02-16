635f40328431cf50885e316617f4c3607aecd63b
zhang tian <975571814@qq.com>
2017-02-16 13:08:37 +0800

    get block title,content,time and author

--- public/js/components/write-block.js
+++ public/js/components/write-block.js
@@ -1,7 +1,34 @@
 import React, {Component} from 'react';
 
 export default class WriteBlock extends Component {
+    addBlock() {
+        const title = this.refs.blockTitle.value;
+        const content = document.getElementById("blockContent").value;
+        const time = this.refs.blockTime.value;
+        const author = this.refs.blockAuthor.value;
+        console.log(title, content, time, author);
+    }
     render() {
-        return <div>write block</div>
+        return <div>
+            <div>
+                标题：
+                <input type="text" id="blockTitle" ref="blockTitle"/>
+            </div>
+            <div>
+                内容：
+                <textarea name="blockContent" id="blockContent" cols="30" rows="5" ref="blockContent"></textarea>
+            </div>
+            <div>
+                时间：
+                <input type="text" id="blockTime" ref="blockTime"/>
+            </div>
+            <div>
+                作者：
+                <input type="text" id="blockAuthor" ref="blockAuthor"/>
+            </div>
+            <div>
+                <button onClick={this.addBlock.bind(this)}>完成</button>
+            </div>
+        </div>
     }
 }
