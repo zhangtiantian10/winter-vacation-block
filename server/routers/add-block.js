1c99cb4b1c7222f2036fa29d0254f38732b59bd4
liyanyan19940914 <921258351@qq.com>
2017-02-17 17:13:49 +0800

    modify users client routers

--- server/routers/add-block.js
+++ server/routers/add-block.js
@@ -2,21 +2,27 @@ const express = require('express');
 const router = express.Router();
 
 let datas = [];
-let useName = [];
 
 router.post('/addBlock', (req, res) => {
     datas.push(req.body);
     console.log(datas);
     res.status(201).end();
 });
-router.post('/addUseName',(req,res) => {
-    useName.push(req.body);
-    console.log(useName);
-    res.status(201).end();
-})
 
 router.get('/getAllBlocks', (req, res) => {
     res.send({blocks: datas});
+});
+
+let users = [];
+
+router.post('/addUseName',(req,res) => {
+    users.push(req.body);
+    console.log(users);
+    res.status(201).end();
+})
+
+router.get('/getAllUsers', (req, res) => {
+    res.send({users: users});
 });
 
 module.exports = router;
