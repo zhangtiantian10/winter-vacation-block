cc01aadac10fe8a0353736e5ef011ee27bb8ed54
zhang tian <975571814@qq.com>
2017-02-17 17:12:07 +0800

    add a block at server and add style to block list

--- public/js/components/home.js
+++ public/js/components/home.js
@@ -6,14 +6,20 @@ export default class Home extends Component {
     }
 
     render() {
-        console.log(this.props.blocks);
         return <div>
-            博客列表：
-            {this.props.blocks.map((b,i) => {
-                return <div key={i}>
-                    {b.title},{b.content},{b.time},{b.author}
-                </div>
-            })}
+            <div className="col-md-10 col-md-offset-1">
+                <h3>博客列表：</h3>
+                {this.props.blocks.map((b, i) => {
+                    return <div key={i}>
+                        <div>
+                            <h3>
+                                <a href="#">{b.title}</a>
+                                <small className="col-md-offset-1">{b.author + "   " + b.time}</small>
+                            </h3>
+                        </div>
+                    </div>
+                })}
+            </div>
         </div>
     }
 }
--- server/routers/add-block.js
+++ server/routers/add-block.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-let datas = [];
+let datas = [
+    {
+        title: "今天真的很开心",
+        content: "因为今天放假了，可以好好的睡懒觉了真的很开心",
+        time: "2017-2-17 16:40",
+        author: "张甜"
+    }
+];
 
 router.post('/addBlock', (req, res) => {
     datas.push(req.body);
