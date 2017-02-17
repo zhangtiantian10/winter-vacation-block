502c3f5c8edb4b3316345648953a0b078a3f2502
zhang tian <975571814@qq.com>
2017-02-17 14:17:15 +0800

    start home view display block list

--- public/js/main.js
+++ public/js/main.js
@@ -3,21 +3,21 @@ import {render} from "react-dom";
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from "react-redux";
 import {Router, Route, IndexRoute, browserHistory} from 'react-router';
-import App from "./containers/App";
 import WriteBlock from "./containers/write-block";
 import reducer from "./reducer/index"
 import middleAddBlock from "./middlewares/add-block";
 import Register from "./containers/register";
 import middleAddUseName from "./middlewares/register";
+import Home from './containers/home';
 
-const createStoreWithMiddleware = applyMiddleware(middleAddBlock,middleAddUseName)(createStore);
+const createStoreWithMiddleware = applyMiddleware(middleAddBlock, middleAddUseName)(createStore);
 
 const store = createStoreWithMiddleware(reducer);
 
 render(
     <Provider store={store}>
         <Router history={browserHistory}>
-            <Route path="/" component={App}>
+            <Route path="/" component={Home}>
             </Route>
             <Route path="writeBlock" component={WriteBlock}/>
             <Route path="register" component={Register}/>
