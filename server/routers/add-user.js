4c636f07142d7b208b1275bdd88f55ec1f90256f
liyanyan19940914 <921258351@qq.com>
2017-02-19 16:20:58 +0800

    verify userInformation

--- server/routers/add-user.js
+++ server/routers/add-user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-let users = [];
+let users = [{userName:"liyan",password:'123'}];
 
 router.post('/addUseName',(req,res) => {
     users.push(req.body);
@@ -9,8 +9,17 @@ router.post('/addUseName',(req,res) => {
     res.status(201).end();
 })
 
-router.get('/getAllUsers', (req, res) => {
-    res.send({users: users});
+router.post('/section', function (req, res) {
+    const userInformation = req.body;
+    users.map((user) => {
+        if(user.password != userInformation.password && user.useName != userInformation.password){
+            res.json({value: "用户名或密码错误，请重新输入"});
+        }
+        else{
+            res.json({value: "登录成功"});
+        }
+    });
+
 });
 
 module.exports = router;
