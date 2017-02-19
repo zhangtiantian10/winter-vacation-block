a792f85a6acf38a13e1561aa86b78b984b0a1991
liyanyan19940914 <921258351@qq.com>
2017-02-19 16:19:55 +0800

    middleware can gain logi's data

--- public/js/middlewares/get-users.js
+++ public/js/middlewares/get-users.js
@@ -1,13 +1,22 @@
-import request from 'superagent';
+import request from "superagent";
 
-export default store => next => action => {
-    console.log('login2');
-    if(action.type === "ALL_USERS") {
-        request.get("/addUser/getAllUsers")
-            .end((err,res) => {
-                next({type: "GET_ALLUSERS", users: res.body.users});
-            });
-    } else{
-        next(action);
+export default store=> next=> action => {
+    console.log("store");
+    if (action.type === "LOGIN") {
+        console.log("action");
+        if(action.userName === ''){
+            alert('用户名不能为空')
+        }
+        else if (action.password === '') {
+            alert("密码不能为空");
+        }
+        else {
+            request.post('/addUser/section')
+                .send({name:action.userName, password:action.password})
+                .end((err, res) => {
+                    next({type: 'LOGIN', status: res.body.value})
+                });
+        }
     }
 }
+
