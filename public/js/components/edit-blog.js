1fa5c5b6141a475cd2391ab33a0ced5f9c5cdefd
zhang tian <975571814@qq.com>
2017-02-20 18:38:53 +0800

    edit blog view is complete

--- public/js/components/edit-blog.js
+++ public/js/components/edit-blog.js
@@ -1,9 +1,48 @@
 import React, {Component} from 'react';
 
 export default class EditBlog extends Component {
-    render(){
-        return <div>
-            修改博客
+    componentDidMount() {
+        const blog = this.props.location.query;
+        document.getElementById('title').value = blog.title;
+        document.getElementById('content').value = blog.content;
+        document.getElementById('time').value = blog.time;
+        document.getElementById('author').value = blog.author;
+    }
+
+    render() {
+        return <div className="col-md-6 col-md-offset-3">
+            <div className="center">
+                <h1>修改博客</h1>
+            </div>
+            <form className="form-horizontal">
+                <div className="form-group">
+                    <label className="col-md-2 control-label">博客名称</label>
+                    <div className="col-md-10">
+                        <input type="text" className="form-control" id="title"/>
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label className="col-md-2 control-label">博客内容</label>
+                    <div className="col-md-10">
+                        <textarea className="form-control" rows="12" id="content"></textarea>
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label className="col-md-2 control-label">修改时间</label>
+                    <div className="col-md-4">
+                        <input type="text" className="form-control" id="time"/>
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label className="col-md-2 control-label">修改人</label>
+                    <div className="col-md-4">
+                        <input type="text" className="form-control" id="author"/>
+                    </div>
+                </div>
+            </form>
+            <div className="center">
+                <button className="btn btn-default">保存</button>
+            </div>
         </div>
     }
 }
