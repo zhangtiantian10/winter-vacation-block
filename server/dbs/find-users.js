fa4092b075ade0e9593294548af0ffdd7c4ec478
liyanyan19940914 <921258351@qq.com>
2017-02-20 17:57:08 +0800

    find user

--- server/dbs/find-users.js
+++ server/dbs/find-users.js
@@ -1,3 +1,16 @@
-/**
- * Created by liyan on 17-2-20.
- */
+const url = require('../helps/get-url');
+const MongoClient = require('../helps/mongodb');
+
+function findUsers(userInformation, callback) {
+    MongoClient.connect(url, function (err, db) {
+        const collection = db.collection('users');
+        const userName = userInformation.useName;
+
+        collection.find({useName:userName}).toArray(function (err, result) {
+            console.log(result);
+            callback(result);
+        });
+    })
+}
+
+module.exports = findUsers;
