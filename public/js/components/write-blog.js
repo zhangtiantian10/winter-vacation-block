1b034a3d3d78efc3730d04d86f38668ecb523a85
zhang tian <975571814@qq.com>
2017-02-22 11:59:55 +0800

    Add style to write blog view

--- public/js/components/write-blog.js
+++ public/js/components/write-blog.js
@@ -3,10 +3,10 @@ import {browserHistory} from 'react-router';
 
 export default class WriteBlock extends Component {
     addBlock() {
-        const title = this.refs.blockTitle.value;
-        const content = document.getElementById("blockContent").value;
-        const time = this.refs.blockTime.value;
-        const author = this.refs.blockAuthor.value;
+        const title = this.refs.blogTitle.value;
+        const content = document.getElementById("blogContent").value;
+        const time = this.refs.blogTime.value;
+        const author = this.refs.blogAuthor.value;
         console.log(title, content, time, author);
         this.props.onSubmit({title, time, content, author});
     }
@@ -18,25 +18,36 @@ export default class WriteBlock extends Component {
     }
 
     render() {
-        return <div>
-            <div>
-                标题：
-                <input type="text" id="blockTitle" ref="blockTitle"/>
-            </div>
-            <div>
-                内容：
-                <textarea name="blockContent" id="blockContent" cols="30" rows="5" ref="blockContent"></textarea>
-            </div>
-            <div>
-                时间：
-                <input type="text" id="blockTime" ref="blockTime"/>
-            </div>
-            <div>
-                作者：
-                <input type="text" id="blockAuthor" ref="blockAuthor"/>
-            </div>
-            <div>
-                <button onClick={this.addBlock.bind(this)}>完成</button>
+        return <div className="col-md-6 col-md-offset-3">
+            <h1 className="center">写博客</h1>
+            <form className="form-horizontal">
+                <div className="form-group">
+                    <label className="col-md-2 control-label">博客名称</label>
+                    <div className="col-md-10">
+                        <input type="text" className="form-control" id="title" ref="blogTitle"/>
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label className="col-md-2 control-label">博客内容</label>
+                    <div className="col-md-10">
+                        <textarea className="form-control" rows="12" id="blogContent"></textarea>
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label className="col-md-2 control-label">时间</label>
+                    <div className="col-md-4">
+                        <input type="text" className="form-control" id="time" ref="blogTime"/>
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label className="col-md-2 control-label">作者</label>
+                    <div className="col-md-4">
+                        <input type="text" className="form-control" id="author" ref="blogAuthor"/>
+                    </div>
+                </div>
+            </form>
+            <div className="center">
+                <button className="btn btn-default" onClick={this.addBlock.bind(this)}>完成</button>
             </div>
         </div>
     }
